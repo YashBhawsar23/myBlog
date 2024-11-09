497268e2f2d4bb7738c08c4dfdc33964e57e8ae4
Yash Bhawsar <yashbhawsar97@gmail.com>
2024-11-09 22:28:58 +0530

    responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <>
+      {/* Updates */}
       <footer className="px-4 divide-y dark:bg-gray-100 dark:text-gray-800">
         <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
           <div className="lg:w-1/3">
@@ -22,7 +23,7 @@ const Footer = () => {
                 </svg>
               </div>
               <span className="self-center text-2xl font-semibold">
-                Brand name
+                Insightly
               </span>
             </a>
           </div>
